488f20635c67f36f908d02b2d5c5fac8b789b847
Joao Daniel <joaodanieldev@gmail.com>
2021-05-14 20:08:39 -0300

    lista categorias e filtro de tipo categorias

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -1,8 +1,10 @@
 import ListRestaurants from "../../components/ListRestaurants";
+import Category from '../../components/Categories'
 
 export default function Restaurants() {
  return (
    <>
+     <Category />
      <ListRestaurants />
    </>
 )
